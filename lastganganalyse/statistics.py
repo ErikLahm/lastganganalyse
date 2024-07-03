e01326f6dc9328aba963050c6984b1949cd0a5e7
Erik Weilandt <erik.weilandt@tum.de>
2024-07-03 19:00:51 +0200

    add more statistics

--- lastganganalyse/statistics.py
+++ lastganganalyse/statistics.py
@@ -30,13 +30,20 @@ class MonthlyStatistics:
     def calculate_monthly_sum(self) -> None:
         """Calculate the sum of values for each month."""
         self.combined_df["Month"] = self.combined_df.iloc[:, 0].dt.to_period("M")
+        # dividing by 4 comes from the interval 15min, so this must be turned into a variable
+        # here calculating from kW to kWh?
         monthly_sum: Series = (
             self.combined_df.groupby("Month")[self.combined_df.columns[1]].sum() / 4
         )
         self.stats_df = self.stats_df.merge(
             monthly_sum, how="left", left_on="Month", right_index=True
         )
-        self.stats_df.rename(columns={self.stats_df.columns[1]: "Sum"}, inplace=True)
+        self.stats_df.rename(
+            columns={self.stats_df.columns[1]: "Verbrauch pro Monat"}, inplace=True
+        )
+        self.stats_df["Verbrauch pro Monat"] = self.stats_df[
+            "Verbrauch pro Monat"
+        ].round(0)
 
     def calculate_monthly_max(self) -> None:
         """Calculate the maximum value for each month."""
@@ -44,7 +51,9 @@ class MonthlyStatistics:
         self.stats_df = self.stats_df.merge(
             monthly_max, how="left", left_on="Month", right_index=True
         )
-        self.stats_df.rename(columns={self.stats_df.columns[2]: "Max"}, inplace=True)
+        self.stats_df.rename(
+            columns={self.stats_df.columns[2]: "Höchster Wert pro Monat"}, inplace=True
+        )
 
     def count_values_above_threshold(self, threshold: float) -> None:
         """Count how often values per month are above a given threshold."""
@@ -59,16 +68,53 @@ class MonthlyStatistics:
             counts_above_threshold, how="left", left_on="Month", right_index=True
         ).fillna(0)
         self.stats_df.rename(
-            columns={self.stats_df.columns[3]: "Count_Above_Threshold"}, inplace=True
+            columns={self.stats_df.columns[3]: f"Ereignisse über {self.threshold} kW"},
+            inplace=True,
         )
+
+    def count_values_below_threshold(self, threshold: float) -> None:
+        """Count how often values per month are below a given threshold."""
+        self.threshold = threshold
+        counts_above_threshold: Series = (
+            self.combined_df[self.combined_df.iloc[:, 1] <= self.threshold]
+            .groupby("Month")
+            .count()
+            .iloc[:, 0]
+        )
+        self.stats_df = self.stats_df.merge(
+            counts_above_threshold, how="left", left_on="Month", right_index=True
+        ).fillna(0)
+        self.stats_df.rename(
+            columns={self.stats_df.columns[4]: f"Ereignisse unter {self.threshold} kW"},
+            inplace=True,
+        )
+
+    def calculate_percentage(self) -> None:
+        """Calculate the percetage between the number of events below threshold"""
+        self.stats_df["in %"] = (
+            self.stats_df.iloc[:, 3]
+            / (self.stats_df.iloc[:, 3] + self.stats_df.iloc[:, 4])
+            * 100
+        )
+        self.stats_df["in %"] = self.stats_df["in %"].round(2)
 
     def calculate_ratio_of_sum_to_max(self) -> None:
         """Create a new column in stats_df that is the ratio of the 'Sum' column to the 'Max' column, handling division by 0."""
         # Use a lambda function to safely handle division, replacing division by 0 with None (or another chosen value)
-        self.stats_df["Sum_to_Max_Ratio"] = self.stats_df.apply(
-            lambda row: row["Sum"] / row["Max"] if row["Max"] != 0 else None, axis=1
+        self.stats_df["VBH"] = self.stats_df.apply(
+            lambda row: (
+                row[self.stats_df.columns[1]] / row[self.stats_df.columns[2]]
+                if row[self.stats_df.columns[2]] != 0
+                else None
+            ),
+            axis=1,
         )
+        self.stats_df["VBH"] = self.stats_df["VBH"].round(0)
 
     def get_stats_df(self) -> DataFrame:
         """Return the statistics DataFrame."""
         return self.stats_df
+
+    def get_raw_df(self) -> DataFrame:
+        """Return the raw data as one DataFrame"""
+        return self.combined_df
